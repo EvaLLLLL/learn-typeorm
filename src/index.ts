3fe29ec1819e6f783315d8900463af59339ac570
lyhhh <17610603221@163.com>
2022-05-30 00:22:07 +0800

    使用 QueryBuilder

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import { Photo } from './entity/Photo'
 
 createConnection()
   .then(async connection => {
-    let photoRepository = await connection.getRepository(Photo)
-    let photo = await photoRepository.find({ relations: ['metadata'] })
-   
-    console.log(photo)
+    let photos = await connection
+      .getRepository(Photo)
+      .createQueryBuilder('photo')
+      .innerJoinAndSelect('photo.metadata', 'metadata')
+      .getMany()
+
+    console.log(photos)
   })
   .catch(error => console.log(error))
