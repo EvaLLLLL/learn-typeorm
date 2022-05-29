c1e2cfd7103837bf8b69daa154f16ff9d5ca2df3
lyhhh <17610603221@163.com>
2022-05-29 20:59:40 +0800

    test Repositories

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,22 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-import { User } from './entity/User'
 import { Photo } from './entity/Photo'
 
 createConnection()
   .then(async connection => {
-    console.log('Inserting a new user into the database...')
-    const user = new User()
-    user.firstName = 'Eva'
-    user.lastName = 'Liu'
-    user.age = 25
-    await connection.manager.save(user)
-    console.log('Saved a new user with id: ' + user.id)
-
-    console.log('Loading users from the database...')
-    const users = await connection.manager.find(User)
-    console.log('Loaded users: ', users)
-
     let photo = new Photo()
-    photo.name = 'Me and Bears'
-    photo.description = 'I am near polar bears'
-    photo.filename = 'photo-with-bears.jpg'
-    photo.views = 1
+    photo.name = 'test repositories'
+    photo.description = 'hahahahahaha'
+    photo.filename = 'test.jpg'
+    photo.views = 20
     photo.isPublished = true
-    const savedPhoto = await connection.manager.save(photo)
-    console.log('Photo has been saved. Photo id is', savedPhoto.id)
-    const allSavedPhoto = await connection.manager.find(Photo)
-    console.log('all saved photo: ', allSavedPhoto)
+
+    let photoRepository = connection.getRepository(Photo)
+
+    await photoRepository.save(photo)
+    console.log(`#${photo.id} photo has been saved`)
+
+    let savedPhotos = await photoRepository.find()
+    console.log('All photos from the db: ', savedPhotos)
   })
   .catch(error => console.log(error))
