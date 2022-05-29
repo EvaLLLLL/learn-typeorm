bd6b1217220358197c46bb6ae4307443664147b7
lyhhh <17610603221@163.com>
2022-05-29 21:07:35 +0800

    更新数据

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,8 @@ import { Photo } from './entity/Photo'
 createConnection()
   .then(async connection => {
     let photoRepository = connection.getRepository(Photo)
-    let allPhotos = await photoRepository.find()
-    console.log('All photos from the db: ', allPhotos)
-
-    let firstPhoto = await photoRepository.findOne(1)
-    console.log('First photo from the db: ', firstPhoto)
-
-    let meAndBearsPhoto = await photoRepository.findOne({
-      name: 'Me and Bears',
-    })
-    console.log('Me and Bears photo from the db: ', meAndBearsPhoto)
-
-    let allViewedPhotos = await photoRepository.find({ views: 1 })
-    console.log('All viewed photos: ', allViewedPhotos)
-
-    let allPublishedPhotos = await photoRepository.find({ isPublished: true })
-    console.log('All published photos: ', allPublishedPhotos)
-
-    let [allps, photosCount] = await photoRepository.findAndCount()
-    console.log('All photos: ', allps)
-    console.log('Photos count: ', photosCount)
+    let photoToUpdate = await photoRepository.findOne(1)
+    photoToUpdate.name = 'hi'
+    await photoRepository.save(photoToUpdate)
   })
   .catch(error => console.log(error))
