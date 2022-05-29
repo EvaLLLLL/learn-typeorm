a5a45e9788fe012fde7799eee401a23283e0d811
lyhhh <17610603221@163.com>
2022-05-30 00:20:17 +0800

    查询关系对象的数据

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,12 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 import { Photo } from './entity/Photo'
-import { PhotoMetadata } from './entity/PhotoMetadata'
 
 createConnection()
   .then(async connection => {
-    // 创建 photo
-    let photo = new Photo()
-    photo.name = 'Me and Bears'
-    photo.description = 'I am near polar bears'
-    photo.views = 33
-    photo.filename = 'photo-with-bears.jpg'
-    photo.isPublished = true
-
-    // 创建 photo metadata
-    let metadata = new PhotoMetadata()
-    metadata.height = 640
-    metadata.width = 480
-    metadata.compressed = true
-    metadata.comment = 'cybershoot'
-    metadata.orientation = 'portait'
-    metadata.photo = photo // 联接两者
-
-    // 获取实体 repositories
-    let photoRepository = connection.getRepository(Photo)
-    let metadataRepository = connection.getRepository(PhotoMetadata)
-
-    // 先保存photo
-    await photoRepository.save(photo)
-
-    // 然后保存photo的metadata
-    await metadataRepository.save(metadata)
-
-    // 完成
-    console.log(
-      'Metadata is saved, and relation between metadata and photo is created in the database too',
-    )
+    let photoRepository = await connection.getRepository(Photo)
+    let photo = await photoRepository.find({ relations: ['metadata'] })
+   
+    console.log(photo)
   })
   .catch(error => console.log(error))
