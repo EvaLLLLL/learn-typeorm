f36c01cf57799a0a7a9a3146883027616103aa17
lyhhh <17610603221@163.com>
2022-05-30 14:42:56 +0800

    查询关系

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,13 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 import { Photo } from './entity/Photo'
-import { PhotoMetadata } from './entity/PhotoMetadata'
-import { Author } from './entity/Author'
-import { Album } from './entity/Album'
 
 createConnection()
   .then(async connection => {
-    // 创建 photo 对象
-    let photo = new Photo()
-    photo.name = 'Me and Bears'
-    photo.description = 'I am near polar bears'
-    photo.filename = 'photo-with-bears.jpg'
-    photo.isPublished = true
-    photo.views = 233
+    const loadedPhoto = await connection
+      .getRepository(Photo)
+      .findOne({ relations: ['albums'] })
 
-    let ellen = new Author()
-    ellen.name = 'ellen'
-    photo.author = ellen
-
-    // 创建 photo metadata 对象
-    let metadata = new PhotoMetadata()
-    metadata.height = 640
-    metadata.width = 480
-    metadata.compressed = true
-    metadata.comment = 'cybershoot'
-    metadata.orientation = 'portait'
-
-    photo.metadata = metadata // this way we connect them
-
-    let album = new Album()
-    album.name = 'chapter-1'
-    album.photos = [photo]
-
-    // 获取 repository
-    let photoRepository = connection.getRepository(Photo)
-
-    // 保存photo的同时保存metadata
-    await photoRepository.save(photo)
-
-    console.log('Photo is saved, photo metadata is saved too.')
+    console.log(loadedPhoto)
   })
   .catch(error => console.log(error))
